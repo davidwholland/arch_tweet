3424c49133fadd5f8d56844b407c4f8ff31da475
David Holland <dave.holl@gmail.com>
2013-02-13 20:42:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArchTweet::Application.config.secret_token = 'c9a5bba8a389fff149e01d58939c21560887c2272ed3203937b2a46692584036171b85baf88abde84412075d837f4227b5b6125015281bc850dd8a43a174b8cb'
